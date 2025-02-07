d5c8c4476d80b231e469dd1d8b3c5d111c8c5d23
fazilajafarli <fezile.ceferli96@gmail.com>
2025-02-07 12:12:22 +0400

    create Article model and serializer

--- article_manager/articles/models.py
+++ article_manager/articles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    author = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def save(self, *args, **kwargs):
+        if len(self.content.split()) > 2000:
+            raise ValidationError('Content should contain max 2000 words.')
+        super().save(*args, **kwargs)
+
+
